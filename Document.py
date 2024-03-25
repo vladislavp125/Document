adca005e33b57e987d9834eff8a6c8c1c18c6528
LOKI LOKI <vladislavp2011@gmail.com>
2024-03-25 18:14:09 +0300

    Добавил возможность передавать текст документа и обновил тесты

--- Document.py
+++ Document.py
@@ -2,22 +2,24 @@ class Document:
     def __init__(self, printer):
         self._printer = printer
 
-    def print_document(self):
-        self._printer.print_page()
+    def print_document(self, text):
+        self._printer.print_page(text)
 
 
 class InkjetPrinter:
-    def print_page(self):
+    def print_page(self, text):
         print("Документ был напечатан на 'Струйном принтере'")
+        print(text)
 
 
 class LaserPrinter:
-    def print_page(self):
+    def print_page(self, text):
         print("Документ был напечатан на 'Лазерном принтере'")
+        print(text)
 
 
 if __name__ == "__main__":
-
+    text = 'Текст документа'
     #Создаем объекты стратегий
     inkjekt_printer = InkjetPrinter()
     laser_printer = LaserPrinter()
@@ -27,6 +29,6 @@ if __name__ == "__main__":
     document2 = Document(inkjekt_printer)
 
     #Печатаем
-    document1.print_document()
-    document2.print_document()
+    document1.print_document(text)
+    document2.print_document(text)
 
--- test_Document.py
+++ test_Document.py
@@ -3,20 +3,27 @@ from unittest.mock import patch
 from Document import Document, InkjetPrinter, LaserPrinter
 
 
+text = 'Текст документа'
+
+
 class TestDocument(unittest.TestCase):
     def test_injkt(self):
+        text = 'Текст документа'
         injkt = InkjetPrinter()
         doc = Document(injkt)
         with patch('builtins.print') as mocked_print:
-            doc.print_document()
-            mocked_print.assert_called_once_with("Документ был напечатан на 'Струйном принтере'")
+            doc.print_document(text)
+            mocked_print.assert_any_call("Документ был напечатан на 'Струйном принтере'")
+            mocked_print.assert_any_call('Текст документа')
 
     def test_laser(self):
+        text = 'Текст документа'
         laser = LaserPrinter()
         doc1 = Document(laser)
         with patch('builtins.print') as mocked_print:
-            doc1.print_document()
-            mocked_print.assert_called_once_with("Документ был напечатан на 'Лазерном принтере'")
+            doc1.print_document(text)
+            mocked_print.assert_any_call("Документ был напечатан на 'Лазерном принтере'")
+            mocked_print.assert_any_call('Текст документа')
 
 
 if __name__ == "__main__":
